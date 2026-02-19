a79225f77ffeb766450f8cc2db1968b022ef2441
emaldev <261611436+emaldev@users.noreply.github.com>
2026-02-19 07:48:05 +0430

    Max 2d array pointer

--- Static_memory.cpp
+++ Static_memory.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    cout << "Enter array size :"; 
     int sum = 0;
     int arr[3][3];
     int (*ptr)[3] = arr;
@@ -12,14 +13,20 @@ int main()
             cin >> *(*(ptr + i) + j);
         }
     }
+      int max = ptr[0][0];
 
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
+            if(ptr[i][j] > max) 
+            max = ptr[i][j];
             sum += *(*(ptr + i) + j );
+            
         }
     }
 
-    cout << sum;
+    cout <<"Sum :" << sum << endl;
+    cout <<"Max :" << max << endl;
+    
 
     return 0;
 }
