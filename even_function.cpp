8caf225393620da61d4d9155a9ede18fb1b2aa06
emaldev <261611436+emaldev@users.noreply.github.com>
2026-02-21 23:47:48 +0430

    max number of function

--- even_function.cpp
+++ even_function.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int square(int x){
+int square(int x ){
     return x * x;
 }
-bool isEven(int x){
-    return x % 2 == 0;
+int Max(int x , int y){
+    if(x > y)
+    return x;
+    else 
+    return y;
 }
 int main(){
-   int number;
-   cout << "Enter a number :> ";
-   cin >> number;
-   if(isEven(number)){
-    cout << "Number is even" << endl;
+ int number1, number2;
+ cout << "Enter 2 number :" << endl;
+ cin >> number1 >> number2;
+
+ 
+   if(number1 > number2){
+    cout << "number1 is larger then number2" << endl;
    }
-   else{
-    cout << "Number is odd" << endl;
-   }
-   cout << "Spuare is :" << square(number) << endl;
-    return 0;
+ 
+ else if(number2 > number1){
+    cout << "number2 is larger then number1" << endl;
+ }
+ else{
+    cout << "Both numbers are equal" << endl;
+ }
+  cout << "Max is:" << Max(number1, number2) << endl;
+  cout << "Square of " << number1 << " is:" << square(number1) << endl;
+
+ return 0;
 }
