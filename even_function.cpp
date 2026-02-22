a483baabd02b021732366c0da2798ae7c9d57712
emaldev <261611436+emaldev@users.noreply.github.com>
2026-02-22 22:26:56 +0430

    function

--- even_function.cpp
+++ even_function.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int square(int x ){
-    return x * x;
-}
-int Max(int x , int y){
-    if(x > y)
-    return x;
-    else 
-    return y;
-}
 int main(){
- int number1, number2;
- cout << "Enter 2 number :" << endl;
- cin >> number1 >> number2;
+  int number1, number2;
+  cout << "Enter 2 number "<< endl;
+  cin >> number1 >> number2;
+  char  op;
+  cout << "Enter a operator (+ , - , * , /):";
+  cin >> op;
 
- 
-   if(number1 > number2){
-    cout << "number1 is larger then number2" << endl;
-   }
- 
- else if(number2 > number1){
-    cout << "number2 is larger then number1" << endl;
- }
- else{
-    cout << "Both numbers are equal" << endl;
- }
-  cout << "Max is:" << Max(number1, number2) << endl;
-  cout << "Square of " << number1 << " is:" << square(number1) << endl;
 
- return 0;
+
+   switch(op){
+    case '+':
+        cout << number1 << " + " << number2 << "=" << number1 + number2;
+        break;
+    case '-':
+       cout << number1 << " - " << number2 << "=" << number1 - number2;
+       break;
+    case '*':
+      cout << number1 << " * " << number2 << "=" << number1 * number2;
+      break;
+    case '/':
+       if(number2 != 0){
+         cout << number1 << " / " << number2 << "=" << number1 / number2;
+       }else{
+         cout << "Error : Division by zero!";
+       }
+       
+       break;
+       default:
+         cout << "Error: Invalid operator!";
+  }
+  return 0;
 }
