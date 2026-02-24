7232e28b3becc09d143b0c4f5aadaddfe4970916
emaldev <261611436+emaldev@users.noreply.github.com>
2026-02-24 21:18:34 +0430

    Welcom to C++

---
+++ wecome_to_c++lang.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+  void sayHello(string name)
+      {
+        cout << "=========🙊 + <:::> + 🙊 C++ 🙊  +  <:::> + 🙊========" << endl;
+        cout << "Hello 😁 " << name << "! Welcome back to coding. 😎 " << endl;
+      }
+int main()
+     {
+        string myName = "Emal_Hasanzada";
+
+        sayHello(myName);
+
+        return 0;
+     }
