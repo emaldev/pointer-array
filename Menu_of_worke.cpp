ed5d1ffefedb131a667d564a8d827ebceb00ef1b
emaldev <261611436+emaldev@users.noreply.github.com>
2026-02-24 21:34:20 +0430

    menu of welcom

---
+++ Menu_of_worke.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void Wecome()
+   {
+    cout << "========== + <:::> 🙊 Hello 🙊  <:::> + ===== " << endl;
+    cout << "What is your name my dear?" << endl;
+    cout << "This is a Menu of My worke " << endl;
+    cout << "This is a man of programing " << endl;
+   }
+   void Menu()
+       {
+        cout << "Thear is my page of " << endl;
+       }
+
+       int main()
+       {
+        Menu();
+
+        Wecome();
+        return 0;
+       }
