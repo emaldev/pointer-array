31b7741014e41c43d542f0078f85a5841a317d64
emaldev <261611436+emaldev@users.noreply.github.com>
2026-02-24 20:35:16 +0430

    max

--- age_of_function.cpp
+++ age_of_function.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
-
-    void checkAge(int age)
-      {
-        if(age >= 18){
-            cout << "your age is oK!" << endl;
-        }else{
-            cout << "your age is not ok You are child" << endl;
+   
+   int sumNumber(int a, int b)
+     {
+        if (a > b){
+            cout << "max number is :" << a;
+            return a;
+        }else {
+            cout << "max number is :" << b;
+            return b;
         }
-      }
-int main() 
+     }
+int main()
    {
-         int age;
-         cout << "Please Enter yout age :>";
-         cin >> age;
-         checkAge(age);
-         return 0;
+       int a, b;
+       cout <<"Please Enter 2 number "  << endl;
+            cin >> a >> b;
+       sumNumber(a, b);
+       return 0;
    }
