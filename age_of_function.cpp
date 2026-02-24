90b5d3207eea23e6e69a33225a777df1e1594588
emaldev <261611436+emaldev@users.noreply.github.com>
2026-02-24 20:25:32 +0430

    age

--- age_of_function.cpp
+++ age_of_function.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 using namespace std;
-  
- void checkAge(int age){
-    if(age >= 18)
-    cout << "you are eligible for a licese." << endl;
-    else
-    cout << "yout are too young (child)l" << endl;
-  
- }
 
- 
-int main()
+    void checkAge(int age)
+      {
+        if(age >= 18){
+            cout << "your age is oK!" << endl;
+        }else{
+            cout << "your age is not ok You are child" << endl;
+        }
+      }
+int main() 
    {
-   
-    int sen;
-     cout << "Plese enter your age :";
-    cin >> sen;
-     
-    checkAge(sen);
-   
-    return 0;
-
+         int age;
+         cout << "Please Enter yout age :>";
+         cin >> age;
+         checkAge(age);
+         return 0;
    }
