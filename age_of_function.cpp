9a0e986ecb601c258c9ec8588587292fb93df0fe
emaldev <261611436+emaldev@users.noreply.github.com>
2026-02-24 20:40:00 +0430

    max

--- age_of_function.cpp
+++ age_of_function.cpp
@@ -1,21 +1,22 @@
 #include <iostream>
 using namespace std;
-   
-   int sumNumber(int a, int b)
-     {
-        if (a > b){
-            cout << "max number is :" << a;
-            return a;
-        }else {
-            cout << "max number is :" << b;
-            return b;
-        }
-     }
+
+int findMax(int a, int b){
+    if (a > b){
+        return a;
+    }else {
+        return b;
+    }
+}
+
 int main()
    {
-       int a, b;
-       cout <<"Please Enter 2 number "  << endl;
-            cin >> a >> b;
-       sumNumber(a, b);
-       return 0;
+    int x , y;
+    cout << "Please Enter 2 number :"<< endl;
+    cin >> x  >> y;
+
+    int result = findMax(x, y);
+
+    cout << "The maximum number is :" << result << endl;
+    return 0;
    }
